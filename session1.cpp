d9c0df44f50f2a335727380ea6e336ff748d3390
Michael Lewis <mlewis8@lewisu.edu>
2026-01-14 20:12:43 -0600

    program to find the max

---
+++ session1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+  const int size = 5;
+  int simpleArray[size] = {7,8,10,3,18};
+  int max = simpleArray[0];
+  std::cout << max << std::endl;
+  for (int i = 1; i < size; ++i) {
+    if ( simpleArray[i] > max) {
+      max = simpleArray[i];
+    }
+  }
+
+  std::cout << "The maximum values is " << max << std::endl;
+
+}
